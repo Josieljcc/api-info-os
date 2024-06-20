51a8325470116c0b53175ad40aeb152b537a73d8
Josieljcc <josiel.jcc@hotmail.com>
2024-06-20 19:43:27 -0300

    chore: Add godotenv.Load() to main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/Josieljcc/api-info-os/config"
 	"github.com/Josieljcc/api-info-os/router"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -10,8 +11,13 @@ var (
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		logger.Errorf("godotenv.Load() error: %v", err)
+		return
+	}
 	logger = config.GetLogger("main")
-	err := config.Init()
+	err = config.Init()
 	if err != nil {
 		logger.Errorf("config.Init() error: %v", err)
 		return
