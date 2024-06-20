45e98460538e6749ea0c07c6aba396d6e63efe92
Josieljcc <josiel.jcc@hotmail.com>
2024-06-20 19:43:58 -0300

    chore: Add CORS middleware to router initialization

--- router/router.go
+++ router/router.go
@@ -1,9 +1,13 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func Initialize() {
 	r := gin.Default()
+	r.Use(cors.Default())
 	initializeRoutes(r)
 	r.Run(":8080")
 }
