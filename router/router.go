36de70d5b52f630313a49096948657a0e6a16e03
Josieljcc <josiel.jcc@hotmail.com>
2025-01-18 14:52:12 -0300

    fix: fixes cors configuration

--- router/router.go
+++ router/router.go
@@ -1,13 +1,25 @@
 package router
 
 import (
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Initialize() {
 	r := gin.Default()
-	r.Use(cors.Default())
+
+	config := cors.Config{
+		AllowOrigins:     []string{"https://info-os.shop, http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           24 * time.Hour,
+	}
+
+	r.Use(cors.New(config))
 	initializeRoutes(r)
 	r.Run(":8080")
 }
