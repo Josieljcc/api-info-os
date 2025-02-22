a14fd8f27ef831679a64aa6b298360afdbff66c8
Josieljcc <josiel.jcc@hotmail.com>
2025-02-22 10:10:47 -0300

    feat: add name in service schema

--- schemas/service.go
+++ schemas/service.go
@@ -6,6 +6,7 @@ import (
 
 type Service struct {
 	gorm.Model
+	Name        string  `gorm:"size:100;not null"`
 	Description string  `gorm:"size:255;not null"`
 	Price       float64 `gorm:"not null"`
 	Time        int     `gorm:"size:255;not null"`
@@ -14,6 +15,7 @@ type Service struct {
 
 type ServiceResponse struct {
 	ID          uint    `json:"id"`
+	Name        string  `json:"name"`
 	Description string  `json:"description"`
 	Time        int     `json:"time"`
 	Price       float64 `json:"price"`
@@ -22,6 +24,7 @@ type ServiceResponse struct {
 func (s Service) ToResponse() ServiceResponse {
 	return ServiceResponse{
 		ID:          s.ID,
+		Name:        s.Name,
 		Description: s.Description,
 		Price:       s.Price,
 	}
