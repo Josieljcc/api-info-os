0efdd2cd4350814fba343446b7f6aac42f5863d8
Josieljcc <josiel.jcc@hotmail.com>
2025-07-13 17:56:31 -0300

    feat: create run backup task

---
+++ tasks/dbBackup.go
@@ -0,0 +1,39 @@
+package tasks
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func RunBackup() {
+	dbUser := os.Getenv("MYSQL_USER")
+	dbPassword := os.Getenv("MYSQL_PASSWORD")
+	dbName := os.Getenv("MYSQL_DATABASE")
+	dbHost := "db"
+
+	timestamp := time.Now().Format("2006-01-02_15-04-05")
+	filename := fmt.Sprintf("backup_%s.sql", timestamp)
+
+	cmd := exec.Command("mysqldump",
+		"-h", dbHost,
+		"-u", dbUser,
+		fmt.Sprintf("-p%s", dbPassword),
+		dbName,
+	)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("Erro ao gerar backup: %s\nDetalhes: %v\n", string(output), err)
+		return
+	}
+
+	err = os.WriteFile(fmt.Sprintf("backups/%s", filename), output, 0644)
+	if err != nil {
+		fmt.Printf("Erro ao salvar o backup: %v\n", err)
+		return
+	}
+
+	fmt.Println("Backup automático concluído:", filename)
+}
